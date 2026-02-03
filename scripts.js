3fb5a4b6048f1ea4d4397436f9deaae3e4446177
hieunguyen-design <htnguyen1310@gmail.com>
2026-02-02 22:35:08 -0500

    Add slideshow for Sips wit Mi

---
+++ scripts.js
@@ -0,0 +1,17 @@
+import Swiper from "swiper";
+import "swiper/css";
+
+//Swiper
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
